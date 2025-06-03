adcac362f82233c6bf984eb5010d540c9f504c57
Milan Gauswami <141305766+GAUSWAMIMILAN17@users.noreply.github.com>
2025-06-03 04:22:12 +0000

    container config done

---
+++ JS-1/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treet al js code as newer version
+
+// alert("hello") //we are using nodejs,not browser
+
+console.log(3+3); 
+// code readability should be high
+console.log("hitesh");
+
+let name = "milan"
+let age = 18
+let isLoggedIn = false
+
+// nummber => 2 to power 53
+//bigint
+//string => ""
+// boolean => true / false
+//null => standalone value / null is itself object
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "milan")
